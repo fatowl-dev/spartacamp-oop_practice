ebddf2effd912f5cbaee2a8fbdc4e6b4a1b1422d
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-03 18:01:58 +0900

    計算結果を表示

--- rectangle.py
+++ rectangle.py
@@ -26,18 +26,18 @@ class Rectangle:
         return round(math.sqrt(self.height ** 2 + self.width ** 2), 2)
 
 
-rectangle1 = Rectangle(height=5, width=6)
-assert rectangle1.area() == 30.00
-assert rectangle1.diagonal() == 7.81
-
-rectangle2 = Rectangle(height=3, width=3)
-assert rectangle2.area() == 9.00
-assert rectangle2.diagonal() == 4.24
-
 # rectangle1 = Rectangle(height=5, width=6)
-# rectangle1.area()  # 30.00
-# rectangle1.diagonal()  # 7.81
+# assert rectangle1.area() == 30.00
+# assert rectangle1.diagonal() == 7.81
 #
 # rectangle2 = Rectangle(height=3, width=3)
-# rectangle2.area()  # 9.00
-# rectangle2.diagonal()  # 4.24
+# assert rectangle2.area() == 9.00
+# assert rectangle2.diagonal() == 4.24
+
+rectangle1 = Rectangle(height=5, width=6)
+print(rectangle1.area())  # 30.00
+print(rectangle1.diagonal())  # 7.81
+
+rectangle2 = Rectangle(height=3, width=3)
+print(rectangle2.area())  # 9.00
+print(rectangle2.diagonal())  # 4.24
