af2d549d05b9b82c9eb141b2c8394534ef68a3e9
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-03 17:59:21 +0900

    Rectangleクラスを実装
    - assertによるテストをPass

--- rectangle.py
+++ rectangle.py
@@ -11,6 +11,20 @@
     rectangle2.area()  # 9.00
     rectangle2.diagonal()  # 4.24
 """
+import math
+
+
+class Rectangle:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return round(self.height * self.width, 2)
+
+    def diagonal(self):
+        return round(math.sqrt(self.height ** 2 + self.width ** 2), 2)
+
 
 rectangle1 = Rectangle(height=5, width=6)
 assert rectangle1.area() == 30.00
