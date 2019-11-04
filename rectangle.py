865e53c30c9352b322ab143d41fcbbaa58326683
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-04 09:20:37 +0900

    出力フォーマットが間違っていたので修正

--- rectangle.py
+++ rectangle.py
@@ -34,10 +34,11 @@ class Rectangle:
 # assert rectangle2.area() == 9.00
 # assert rectangle2.diagonal() == 4.24
 
+
 rectangle1 = Rectangle(height=5, width=6)
-print(rectangle1.area())  # 30.00
-print(rectangle1.diagonal())  # 7.81
+print(f'{rectangle1.area():.2f}')  # 30.00
+print(f'{rectangle1.diagonal():.2f}')  # 7.81
 
 rectangle2 = Rectangle(height=3, width=3)
-print(rectangle2.area())  # 9.00
-print(rectangle2.diagonal())  # 4.24
+print(f'{rectangle2.area():.2f}')  # 9.00
+print(f'{rectangle2.diagonal():.2f}')  # 4.24
